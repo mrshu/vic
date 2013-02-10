876335447d98585af9dc8ee4a6ce5d7cd50e8883
mr.Shu <mr@shu.io>
2013-02-10 18:58:00 +0100

    PS1 and intended

--- example.c
+++ example.c
@@ -9,64 +9,65 @@
 
 void test()
 {
-	int t;
-	vic_args("%d", &t);
-	printf("test, args: %s, t = %d\n", vic_buff, t);
-	vic_return("%d", t+1);
+        int t;
+        vic_args("%d", &t);
+        printf("test, args: %s, t = %d\n", vic_buff, t);
+        vic_return("%d", t+1);
 }
 
 void whl()
 {
         char inp[65];
         vic_args("%s", &inp);
-        
+
 
 }
 
 
 void ls()
 {
-	vic_print("listing");
+        vic_print("listing");
 }
 
 void rm()
 {
-	char i[20];
-	vic_args("%s", &i);
-	printf("Removing %s\n", i);
-	vic_exec("ls;");
+        char i[20];
+        vic_args("%s", &i);
+        printf("Removing %s\n", i);
+        vic_exec("ls;");
 
 }
 
 void milis()
 {
-	vic_func();
+        vic_func();
 
-	vic_return("%d", rand());
+        vic_return("%d", rand());
 }
 
 int main(void)
 {
 
-	//vic_fn_add("A", &test);
-	vic_init();
-	
-	vic_fn_add("l", &ls);
-	vic_fn_add("rm", &rm);
-	vic_fn_add("test", &test);
-	vic_fn_add("m", &milis);
+        //vic_fn_add("A", &test);
+        vic_init();
 
-	vic_alias_add("time", "rpc;m;shell;");
-	vic_exec("ls;");
-	while(1){
-		char input[200];	
-		gets(input);
-		int i;
-		for(i = 0; i < strlen(input); i++){
-			vic_process(input[i]);
-		}
-		vic_process('\n');
-	}
+        vic_fn_add("l", &ls);
+        vic_fn_add("rm", &rm);
+        vic_fn_add("test", &test);
+        vic_fn_add("m", &milis);
 
-	return 0;
+        vic_alias_add("time", "rpc;m;shell;");
+        vic_exec("ls;");
+        while(1){
+                char input[200];
+                vic_print("+> ");
+                gets(input);
+                int i;
+                for(i = 0; i < strlen(input); i++){
+                        vic_process(input[i]);
+                }
+                vic_process('\n');
+        }
+
+        return 0;
 }
