aa871e1cf616571bb8a91284996431fd0509b270
mr.Shu <mr@shu.io>
2013-03-23 12:08:43 +0100

    added examples

---
+++ arduino/vic/examples/vicsh/vicsh.ino
@@ -0,0 +1,18 @@
+
+#include <vic.h>
+
+
+void setup 
+{
+  vicore_init(115200);
+  Serial.begin(115200);
+
+}
+
+void loop
+{
+  if(Serial.available()){
+    vicore_process(Serial.read());
+  }
+
+}
