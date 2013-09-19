ac526ede2b40cbd68887162f123a7f505bb4e894
mr.Shu <mr@shu.io>
2013-09-19 19:52:05 +0200

    I/O finally!

---
+++ src/vic-io.c
@@ -0,0 +1,57 @@
+#ifndef _VIC_IO
+#define _VIC_IO
+
+#include "vic.h"
+
+#include <stdlib.h>
+#include <stdint.h>
+
+char* vic_output;
+static uint8_t vic_output_len = 0;
+
+void vic_out(char x)
+{
+    vic_output = (char *)
+            realloc(vic_output,
+                    (vic_output_len + 1) * sizeof(char)
+            );
+    vic_output[vic_output_len++] = x;
+
+    vic_sys_out(x);
+
+}
+
+void vic_print(char *x)
+{
+
+    do {
+        vic_out(*x);
+        x++;
+    } while (*x != '\0');
+
+}
+
+void vic_println(char *x)
+{
+    vic_print(x);
+    vic_out('\n');
+
+}
+
+char* vic_io_return()
+{
+    vic_out('\0');
+    return vic_output;
+}
+
+void vic_io_clean()
+{
+    free(vic_output);
+    vic_output = NULL;
+    vic_output_len = 0;
+}
+
+
+
+#endif
+
