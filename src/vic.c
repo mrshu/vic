5e2bd7d65b0985306a43fb73b96a3b885b4c5be0
VARGAC <simon.varga123@gmail.com>
2015-12-17 18:02:28 +0100

    update: Generalization of func vic_prepare_name
    * Preparing for next modules to be included. They will need function like this.
    * Added parameter which tells to what length shorten the name. It is expected
      to give buffer for new name big enough.
    
    Signed-off-by: VARGAC <simon.varga123@gmail.com>

--- src/vic.c
+++ src/vic.c
@@ -1,5 +1,15 @@
 #include "vic.h"
 
+void vic_prepare_name(const char *raw_name, char *name, int len)
+{
+    memset(name, '\0', len + 1);
+    if (strlen(raw_name) > len) {
+        memcpy(name, raw_name, len);
+    } else {
+        strcpy(name, raw_name);
+    }
+}
+
 void vic_init()
 {
 
